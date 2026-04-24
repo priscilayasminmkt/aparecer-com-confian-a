352583c24fa401298343d29fec813e639b27de3e
priscilayasminmkt <priscilayasmin.mkt@gmail.com>
2026-04-24 10:41:18 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+Sem problema! O código do `main.jsx` é curtinho, copia daqui:
+
+```jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
+
+Cola isso no editor e confirma! 👇
